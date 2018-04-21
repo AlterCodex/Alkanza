13f0148bbd3bd80a3648a9a49e45ad26fc9e2d88
AlterCodex <fjb.pacho@gmail.com>
2018-04-21 12:21:34 -0500

    complete google api consumption

--- alkanza/alkanza/define_urls.py
+++ alkanza/alkanza/define_urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
 	url(r'^$',views.home,name='home'),
+	url(r'^search_place/(?P<place_id>[0-9A-Za-z_\-]+)/(?P<key>[0-9A-Za-z_\-]+)/$',views.search_place,name="search_place"),
 ]
--- alkanza/alkanza/views.py
+++ alkanza/alkanza/views.py
@@ -2,14 +2,28 @@ from django.shortcuts import render, get_object_or_404,redirect
 from django.contrib.sites.shortcuts import get_current_site
 from django.utils.encoding import force_bytes, force_text
 from django.template.loader import render_to_string
-from django.utils.http import urlsafe_base64_encode,urlsafe_base64_decode
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
-from django.contrib.auth import authenticate,login,logout
-from django.utils.decorators import method_decorator
-from django.contrib.auth.decorators import login_required
 from django.http import Http404
-from django.contrib.auth.models import User
 from . import key as KEY
-
+from . import forms 
+from . import google_api_proxy as google_maps
 def home(request):
-    return render(request,'home.html')
+    if request.method== 'POST':
+        form = forms.SearchForm(request.POST)
+        if form.is_valid():
+            google=google_maps.Google_Api()
+            place=form.cleaned_data['search_address']
+            radius=form.cleaned_data['radius']
+            search_text=form.cleaned_data['key_word']
+            (place_id,location)=google.search_place(place)
+            (places,locations)=google.find_nearBy(location,radius,search_text)
+            place_url=""+str(place_id)
+            return redirect('search_place/'+place_url+'/'+KEY.key)
+    return render(request,'home.html',{'seachrForm':forms.SearchForm()})
+
+
+def search_place(request,place_id,key):
+    if request.method == 'GET':
+        return render(request,'search_place.html',{'place_uid':place_id,'key':key})
+    else:
+        raise  Http404("Url not Found")
+    
